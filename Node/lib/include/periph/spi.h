b278897eada5d8542d715bc6a78584a5c7c591f1
Ales Musil <aedvin1@gmail.com>
2019-09-21 19:39:16 +0200

    Node, rfm69: Fix SPI communication
    Signed-off-by: Ales Musil <aedvin1@gmail.com>

--- Node/lib/include/periph/spi.h
+++ Node/lib/include/periph/spi.h
@@ -6,13 +6,10 @@
 #define SPI_CLOCK RCC_SPI1
 #define SPI SPI1
 #define SPI_PORT GPIOA
-#define SPI_NSS GPIO4
 #define SPI_SCK GPIO5
 #define SPI_MISO GPIO6
 #define SPI_MOSI GPIO7
 
 void spi_setup(void);
-
-uint8_t spi_transfer(uint8_t data);
 
 #endif //WEATHER_STATION_SPI_H
--- Node/lib/periph/spi.c
+++ Node/lib/periph/spi.c
@@ -4,7 +4,7 @@ void spi_setup() {
     spi_reset(SPI);
     spi_disable(SPI);
     spi_init_master(SPI,
-                    SPI_CR1_BAUDRATE_FPCLK_DIV_256,
+                    SPI_CR1_BAUDRATE_FPCLK_DIV_32,
                     SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
                     SPI_CR1_CPHA_CLK_TRANSITION_1,
                     SPI_CR1_DFF_8BIT,
@@ -13,12 +13,8 @@ void spi_setup() {
     spi_disable_crc(SPI);
     spi_set_nss_high(SPI);
     spi_enable_software_slave_management(SPI);
+    spi_enable_ss_output(SPI);
+    spi_set_frf_motorola(SPI);
     spi_set_full_duplex_mode(SPI);
     spi_enable(SPI);
-}
-
-uint8_t spi_transfer(uint8_t data) {
-    spi_send(SPI, data);
-    while (SPI_SR(SPI) & SPI_SR_BSY);
-    return (uint8_t) spi_read(SPI);
 }
