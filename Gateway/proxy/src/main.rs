e37dfcaf0c7fddf12f6e8984ff19a8d4b22a4c47
Ales Musil <aedvin1@gmail.com>
2019-12-31 10:24:19 +0100

    Gateway: Move proxy struct into its own module
    Signed-off-by: Ales Musil <aedvin1@gmail.com>

--- Gateway/proxy/src/main.rs
+++ Gateway/proxy/src/main.rs
@@ -1,20 +1,22 @@
-use crate::home_assistant::HomeAssistant;
-use crate::radio::Radio;
-use crate::util::{Receiver, Sender, Shared, PAYLOAD_ON};
+use crate::proxy::Proxy;
+use crate::util::{Sender, Shared};
 use async_std::sync::{Arc, Mutex};
 use async_std::task::block_on;
-use config::{read_conf, Config};
 use ctrlc;
 use futures::channel::mpsc;
-use futures::{select, FutureExt, SinkExt, StreamExt};
-use paho_mqtt::Message;
+use futures::SinkExt;
 
 mod config;
 
 #[macro_use]
 mod util;
 mod home_assistant;
+mod proxy;
 mod radio;
+
+// TODO Proper error handling
+// TODO Logging into file, probably https://docs.rs/crate/simplelog/0.7.4
+// TODO Expression support in config for analog types, probably https://docs.rs/asciimath/0.8.8/asciimath/
 
 #[async_std::main]
 async fn main() {
@@ -35,71 +37,4 @@ fn configure_ctrlc_handler(shared_sender: Shared<Sender<bool>>) {
         block_on(sender.send(true)).unwrap();
     })
     .unwrap();
-}
-
-struct Proxy {
-    conf: Shared<Config>,
-    shutdown: Receiver<bool>,
-    radio: Radio,
-    mqtt: HomeAssistant,
-}
-
-impl Proxy {
-    async fn new(conf_path: &str, shutdown: Receiver<bool>) -> Self {
-        let conf = new_shared!(read_conf(conf_path).await);
-        let radio = Radio::new(conf.clone());
-        let mqtt = HomeAssistant::new(conf.clone()).await;
-        Proxy {
-            conf,
-            shutdown,
-            radio,
-            mqtt,
-        }
-    }
-
-    async fn main_loop(&mut self) {
-        let mut shutdown = (&mut self.shutdown).fuse();
-        let mut receiver = self.radio.receiver_channel();
-        let mut mqtt_receiver = self.mqtt.stream().fuse();
-        loop {
-            select! {
-                opt = receiver.next().fuse() => match opt {
-                    Some(buffer) => self.mqtt.update_state(&buffer).await,
-                    None => println!("Radio channel is closed"),
-                },
-                option = mqtt_receiver.next().fuse() => {
-                    helper_mqtt_config(self.conf.clone(), option).await
-                },
-                sig =  shutdown.next().fuse() => match sig {
-                    Some(_) => break,
-                    None => println!("Shutdown channel is closed, should not happen")
-                },
-            }
-        }
-        receiver.close();
-        println!("Main loop exit");
-    }
-}
-
-async fn helper_mqtt_config(
-    shared_conf: Shared<Config>,
-    wrapper: Option<Result<Option<Message>, ()>>,
-) {
-    match wrapper {
-        Some(result) => {
-            if result.is_err() {
-                println!("MQTT stream error");
-                return;
-            }
-            let message = result.unwrap().unwrap();
-            let mut conf = shared_conf.lock().await;
-            let payload = message.payload_str();
-            if payload.len() == 1 {
-                conf.node_mut()
-                    .update_output(message.topic(), payload == PAYLOAD_ON)
-            }
-            println!("Message {:?}", message);
-        }
-        None => println!("MQTT channel is closed"),
-    }
 }
