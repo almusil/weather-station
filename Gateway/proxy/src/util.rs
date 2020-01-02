6e3842e6e854f16b050ce90df0885c21b0fadceb
Ales Musil <aedvin1@gmail.com>
2020-01-02 13:56:57 +0100

    Gateway: Add value expression into config
    Signed-off-by: Ales Musil <aedvin1@gmail.com>

--- Gateway/proxy/src/util.rs
+++ Gateway/proxy/src/util.rs
@@ -17,9 +17,13 @@ pub const PACKET_CONFIG: u8 = 0x02;
 pub const PACKET_DATA: u8 = 0x08;
 pub const BATTERY_CONFIG_TOPIC: &str = "homeassistant/sensor/node/analog_bat/config";
 pub const BATTERY_STATE_TOPIC: &str = "node/analog/bat/state";
+pub const BATTERY_UNIT: &str = "V";
+pub const BATTERY_EXPR: &str = "{{ (float(value) * 3.3 / (2**12 - 1)) | round(3) }}";
 pub const BATTERY_SENSOR: Discovery = Discovery::Sensor {
     name: "Battery",
     state_topic: BATTERY_STATE_TOPIC,
+    unit_of_measurement: BATTERY_UNIT,
+    value_template: BATTERY_EXPR,
 };
 
 pub type Shared<T> = Arc<Mutex<T>>;
